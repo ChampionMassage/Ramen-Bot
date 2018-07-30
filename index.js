b3e39c6057f5b4f2443c39d45c2cfab7f8667d2a
ChampionMassage <31393363+ChampionMassage@users.noreply.github.com>
2018-07-30 11:29:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,154 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
+let xp = require("./xp.json");
+let purple = botconfig.purple;
+let cooldown = new Set();
+let cdseconds = 5;
+
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+
+  bot.user.setActivity("!Help", {type: "Playing"});
+
+  //bot.user.setGame("on SourceCade!");
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+        prefixes: botconfig.prefix
+    };
+}
+
+if(!coins[message.guild.id]){
+    coins[message.guild.id] = {
+      coins: 0
+    };
+  }
+  let coinAmt = Math.floor(Math.random() * 55) + 1;
+  let baseAmt = Math.floor(Math.random() * 55) + 1;
+  console.log(`${coinAmt} ; ${baseAmt}`);
+
+  if(coinAmt === baseAmt){
+    coins[message.author.id] = {
+      coins: coins[message.author.id].coins + coinAmt
+    };
+  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+    if (err) console.log(err)
+  });
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#0000FF")
+  .addField("💸", `${coinAmt} coins added!`);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  }
+
+
+
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+  let prefix = prefixes[message.guild.id].prefixes;
+  if(!message.content.startsWith(prefix)) return;
+  if(cooldown.has(message.author.id)){
+    message.delete();
+    return message.reply("You have to wait 5 seconds between commands.")
+  }
+  //if(!message.member.hasPermission("ADMINISTRATOR")){
+    cooldown.add(message.author.id);
+ //}
+ 
+
+console.log(prefix);
+
+
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  if(!message.content.startsWith(`${prefix}`)) return;
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+  setTimeout(() => {
+    cooldown.delete(message.author.id)
+  }, cdseconds * 1000)
+
+
+
+  if(cmd === `${prefix}id`)  {
+    const dm = new Discord.RichEmbed()
+    .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+    .setTitle("Successfully fetched ID")
+    .setDescription(`${message.author.tag} your id is ${message.author.id}`)
+    .setColor("#FF0000")
+    .setTimestamp()
+    message.author.send(dm)
+    
+    const embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+    .setTitle("Successfully sent ID")
+    .setDescription("Sent your Discord User ID to your DM's")
+    .setColor("#FF0000")
+    .setTimestamp()
+    .setFooter("Command executed");
+    message.channel.send(embed)
+     } 
+
+});
+
+
+
+bot.login(botconfig.token);
